e923a9b783350925dcfe10ee4e5cc78e9d045624
Dominik Kozaczko <dominik@dworkin>
2013-11-28 17:17:43 +0100

    kopia numerów nagłówkowych

---
+++ rules/pofiles/pl/copyheads.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+
+import re
+
+exp = r'^(?P<numer>msgid "\d{3}\..*)$'
+
+result = []
+first = True
+with open('./comprules.po', 'r') as plik:
+    for line in plik.xreadlines():
+        r = re.match(exp, line)
+        if r:
+            result.append(line)
+            result.append(line.replace('msgid', 'msgstr'))
+            first = False
+        elif first:
+            result.append(line)
+        else:
+            first = True
+
+with open('./cmprls.po', 'w') as zapis:
+    zapis.writelines(result)
