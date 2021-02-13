a2f263a00003c59ac71a2d3bcda0e5d6e37e14a8
Dominik Kozaczko <dominik@kozaczko.info>
2021-02-14 00:47:06 +0100

    2021.2.13

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.2.11',
+    VERSION: '2021.2.13',
     LANGUAGE: 'pl',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
