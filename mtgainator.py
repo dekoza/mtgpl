3745766c36aabb274625d6160c519deba3536e23
Dominik Kozaczko <dominik@kozaczko.info>
2021-02-21 18:35:18 +0100

    Aktualizacja biblioteki Sphinx, odświeżenie plików html

--- mtgainator.py
+++ mtgainator.py
@@ -7,6 +7,7 @@ import polib
 import json
 import hashlib
 import os
+import re
 
 MTGA_DIR = config("MTGA_DIR")
 SUBSTITUTE_LANG = config("SUBSTITUTE_LANG", default="pt-BR")
@@ -19,6 +20,106 @@ _main_lang = SUBSTITUTE_LANG.split("-")[0]
 _debug_lang = SUBSTITUTE_LANG_DEBUG.split("-")[0]
 
 app = typer.Typer()
+rev_map = {
+    "|energy|": "E",
+    "|colorless|": "C",
+    "|tap|": "T",
+    "|untap|": "Q",
+    "|mana_u|": "U",
+    "|mana_w|": "W",
+    "|mana_g|": "G",
+    "|mana_b|": "B",
+    "|mana_r|": "R",
+    "|mana_p|": "P",
+    "|mana_s|": "Si",
+    "|mana_2u|": "(2/U)",
+    "|mana_2w|": "(2/W)",
+    "|mana_2b|": "(2/B)",
+    "|mana_2g|": "(2/G)",
+    "|mana_2r|": "(2/R)",
+    "|mana_up|": "(U/P)",
+    "|mana_wp|": "(W/P)",
+    "|mana_bp|": "(B/P)",
+    "|mana_gp|": "(G/P)",
+    "|mana_rp|": "(R/P)",
+    "|mana_x|": "X",
+    "|mana_0|": "0",
+    "|mana_1|": "1",
+    "|mana_2|": "2",
+    "|mana_3|": "3",
+    "|mana_4|": "4",
+    "|mana_5|": "5",
+    "|mana_6|": "6",
+    "|mana_7|": "7",
+    "|mana_8|": "8",
+    "|mana_9|": "9",
+    "|mana_10|": "10",
+    "|mana_11|": "11",
+    "|mana_12|": "12",
+    "|mana_13|": "13",
+    "|mana_14|": "14",
+    "|mana_15|": "15",
+    "|mana_rg|": "(R/G)",
+    "|mana_rw|": "(R/W)",
+    "|mana_ub|": "(U/B)",
+    "|mana_ur|": "(U/R)",
+    "|mana_wb|": "(W/B)",
+    "|mana_wu|": "(W/U)",
+    "|mana_bg|": "(B/G)",
+    "|mana_br|": "(B/R)",
+    "|mana_gw|": "(G/W)",
+    "|mana_gu|": "(G/U)",
+}
+
+cost_map = {
+    "C": "|colorless|",
+    "T": "|tap|",
+    "Q": "|untap|",
+    "U": "|mana_u|",
+    "W": "|mana_w|",
+    "G": "|mana_g|",
+    "B": "|mana_b|",
+    "R": "|mana_r|",
+    "P": "|mana_p|",
+    "Si": "|mana_s|",
+    "(2/U)": "|mana_2u|",
+    "(2/W)": "|mana_2w|",
+    "(2/B)": "|mana_2b|",
+    "(2/G)": "|mana_2g|",
+    "(2/R)": "|mana_2r|",
+    "(U/P)": "|mana_up|",
+    "(W/P)": "|mana_wp|",
+    "(B/P)": "|mana_bp|",
+    "(G/P)": "|mana_gp|",
+    "(R/P)": "|mana_rp|",
+    "X": "|mana_x|",
+    "0": "|mana_0|",
+    "1": "|mana_1|",
+    "2": "|mana_2|",
+    "3": "|mana_3|",
+    "4": "|mana_4|",
+    "5": "|mana_5|",
+    "6": "|mana_6|",
+    "7": "|mana_7|",
+    "8": "|mana_8|",
+    "9": "|mana_9|",
+    "10": "|mana_10|",
+    "11": "|mana_11|",
+    "12": "|mana_12|",
+    "13": "|mana_13|",
+    "14": "|mana_14|",
+    "15": "|mana_15|",
+    "(R/G)": "|mana_rg|",
+    "(R/W)": "|mana_rw|",
+    "(U/B)": "|mana_ub|",
+    "(U/R)": "|mana_ur|",
+    "(W/B)": "|mana_wb|",
+    "(W/U)": "|mana_wu|",
+    "(B/G)": "|mana_bg|",
+    "(B/R)": "|mana_br|",
+    "(G/W)": "|mana_gw|",
+    "(G/U)": "|mana_gu|",
+}
 
 
 def substr(text: str) -> str:
@@ -56,22 +157,42 @@ def get_valid_mtga_dl_path() -> pathlib.Path:
     return downloads
 
 
-def extract_data_pots():
-    pass
+def extract_data_pots(path: pathlib.Path):
+    mtga_files = (i for i in os.listdir(path) if i.startswith("data_loc_") and i.endswith(".mtga"))
+    pot_path = CURRENT_DIR / "MTG" / "templates"
 
-def get_loc_en_trans(l, loc="en-US"):
-    for i in l:
+    po = polib.POFile()
+    for filename in mtga_files:
+        with open(path / filename) as infile:
+            full_data = json.load(infile)
+            data = get_en_trans(full_data)
+            with typer.progressbar(data, label="Extracting Cards") as progress:
+                for obj in progress:
+                    key = obj["id"]
+                    text = convert_mana_costs(obj["text"])
+                    entry = polib.POEntry(
+                        msgctxt=str(key),
+                        msgid=text,
+                        msgstr="",
+                    )
+                    po.append(entry)
+    po.save(f"{pot_path}/data_loc.pot")
+
+
+def get_loc_en_trans(data, loc="en-US"):
+    for i in data:
         if i["locale"] == loc:
             return i["translation"]
 
-def extract_loc_pots(path:pathlib.Path):
+
+def extract_loc_pots(path: pathlib.Path):
     pot_dir = CURRENT_DIR / "MTGA" / "t"
     mtga_files = (i for i in os.listdir(path) if i.endswith(".mtga"))
 
-    with typer.progressbar(mtga_files, label="Extracting LOC")
-        for filename in mtga_files:
+    with typer.progressbar(mtga_files, label="Extracting Game Locale") as progress:
+        for filename in progress:
             po = polib.POFile()
-            with open(filename) as infile:
+            with open(path / filename) as infile:
                 data = json.load(infile)
                 for idx, obj in enumerate(data, start=1):
                     key = obj["key"]
@@ -108,8 +229,67 @@ def create_datfile(filepath):
         dat_file.write(f"{md5_hash.hexdigest()}\n")
 
 
+def convert_mana_costs(string: str) -> str:
+    energy_pat = r"(\{E+?\})"
+    string = re.sub(energy_pat, convert_energy, string)
+    loyalty_pat = r"(^-(\d+|X):)"
+    string = re.sub(loyalty_pat, convert_loyalty, string)
+    mana_pat = r"(\{o.+?\})"
+    return re.sub(mana_pat, convert_mana, string)
+
+
+def convert_loyalty(match) -> str:
+    s = match.group()
+    return s.replace("-", "–")
+
+
+def convert_mana(match) -> str:
+    s = match.group().strip("{o}")
+    return r"\ ".join(cost_map[cost] for cost in s.split("o"))
+
+
+def convert_energy(match) -> str:
+    s = match.group().strip("{}")
+    return r"\ ".join("|energy|" for i in range(len(s)))
+
+
+def get_en_trans(data, loc="en-US"):
+    for i in data:
+        if i["isoCode"] == loc:
+            return i["keys"]
+
+
 def translate_data(path: pathlib.Path):
-    pass
+    podir = CURRENT_DIR / "translated" / "pl" / "LC_MESSAGES"
+    orig_path = path / "orig"
+
+    mtga_files = (i for i in os.listdir(path) if i.startswith("data_loc") and i.endswith(".mtga"))
+    pofiles = ["data_loc.po"]
+
+    os.makedirs(orig_path, exist_ok=True)
+    for filename in mtga_files:
+        filepath = path / filename
+        bak_filepath = orig_path / filename
+
+        main_name = filename.rsplit("_", 1)[0]
+        poname = f"{main_name}.po"
+        if poname not in pofiles:
+            continue
+
+        if not bak_filepath.exists():
+            shutil.move(filepath, bak_filepath)
+            shutil.move(f"{filepath}.dat", f"{bak_filepath}.dat")
+
+        with open(bak_filepath) as source, open(filepath, "w") as outfile:
+            po = polib.pofile(podir / poname)
+            data = json.load(source)
+
+            do_the_trans(data, po, lang=SUBSTITUTE_LANG)
+            do_the_trans(data, po, lang=SUBSTITUTE_LANG_DEBUG)
+
+            json.dump(fp=outfile, obj=data, ensure_ascii=False, indent=2)
+
+        create_datfile(filepath)
 
 
 def translate_loc(path: pathlib.Path):
@@ -120,7 +300,7 @@ def translate_loc(path: pathlib.Path):
     pofiles = [i for i in os.listdir(popath) if i.endswith(".po")]
 
     os.makedirs(orig_path, exist_ok=True)
-    with typer.progressbar(mtga_files, label="Translating LOC") as progress:
+    with typer.progressbar(mtga_files, label="Translating Game Binary") as progress:
         for filename in progress:
             filepath = path / filename
             bak_filepath = orig_path / filename
@@ -175,8 +355,9 @@ def translate_loc(path: pathlib.Path):
 @app.command(name="extract")
 def extract_pots():
     path = get_valid_mtga_dl_path()
-    extract_loc_pots()
-    extract_data_pots()
+    extract_loc_pots(path / "Loc")
+    extract_data_pots(path / "Data")
+    typer.echo("Done!")
 
 
 @app.command(name="t", hidden=True)
@@ -186,6 +367,7 @@ def translate():
     path = get_valid_mtga_dl_path()
     translate_loc(path / "Loc")
     translate_data(path / "Data")
+    typer.echo("Done!")
 
 
 @app.command(name="b", hidden=True)
@@ -195,7 +377,59 @@ def build():
     # copy original files to tempdir
     # prepare zipfile with translations for distribution
     # save zipfile in `build` dir with proper name
-    pass
+    typer.echo("Done!")
+
+
+def find_data_trans_obj(trans_list, lang="en-US"):
+    for obj in trans_list:
+        if obj["isoCode"] == lang:
+            return obj
+
+
+def revert_costs(string: str) -> str:
+    energy_pat = r"(\|energy(\|\\.)*)+\|"
+    string = re.sub(energy_pat, restore_energy, string)
+    loyalty_pat = r"(^–(\d+|X):)"
+    string = re.sub(loyalty_pat, restore_loyalty, string)
+    mana_pat = r"(\|(mana_[wubrgspx\d]+|colorless|(un)?tap)(\|\\.)*)+\|"
+    return re.sub(mana_pat, restore_mana, string)
+
+
+def restore_energy(match) -> str:
+    s = match.group().split(r"\ ")
+    try:
+        return "{%s}" % "".join(rev_map[cost] for cost in s)
+    except KeyError:
+        print(match.string)
+        print(s)
+        raise
+
+
+def restore_mana(match) -> str:
+    s = match.group().split(r"\ ")
+    try:
+        return "{%s}" % "".join(f"o{rev_map[cost]}" for cost in s)
+    except KeyError:
+        print(match.string)
+        print(s)
+        raise
+
+
+def restore_loyalty(match) -> str:
+    s = match.group()
+    return s.replace("–", "-")
+
+
+def do_the_trans(data, pofile, lang="en-US"):
+    translation_objs = find_data_trans_obj(data, lang=lang)["keys"]
+
+    with typer.progressbar(translation_objs, label=f"Substituting cards for {lang}") as progress:
+        for obj in progress:
+            obj_id = str(obj["id"])
+            po_entry = pofile.find(obj_id, by="msgctxt")
+            if po_entry:
+                text = revert_costs(po_entry.msgstr or po_entry.msgid)
+                obj["text"] = substr(text)
 
 
 if __name__ == "__main__":
