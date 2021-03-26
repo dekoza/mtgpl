2148246b890e834bb68e29a99606333846cd8abb
Dominik Kozaczko <dominik@kozaczko.info>
2021-03-26 15:26:12 +0100

    Aktualizacja

--- mtgdump.py
+++ mtgdump.py
@@ -6,67 +6,7 @@
 import click
 import requests
 
-card_template = """
-:mtgtip:`{card[name]}<{image}|{card_uri}>`
-   {card_text}
-
-"""
-
-symbols_map = {
-    #    '}{': '} {',
-    "{E}": "|energy|",
-    "{C}": "|colorless|",
-    "{T}": "|tap|",
-    "{Q}": "|untap|",
-    "{U}": "|mana_u|",
-    "{W}": "|mana_w|",
-    "{G}": "|mana_g|",
-    "{B}": "|mana_b|",
-    "{R}": "|mana_r|",
-    "{P}": "|mana_p|",
-    "{S}": "|mana_s|",
-    "{2/U}": "|mana_2u|",
-    "{2/W}": "|mana_2w|",
-    "{2/B}": "|mana_2b|",
-    "{2/G}": "|mana_2g|",
-    "{2/R}": "|mana_2r|",
-    "{U/P}": "|mana_up|",
-    "{W/P}": "|mana_wp|",
-    "{B/P}": "|mana_bp|",
-    "{G/P}": "|mana_gp|",
-    "{R/P}": "|mana_rp|",
-    "{X}": "|mana_x|",
-    "{0}": "|mana_0|",
-    "{1}": "|mana_1|",
-    "{2}": "|mana_2|",
-    "{3}": "|mana_3|",
-    "{4}": "|mana_4|",
-    "{5}": "|mana_5|",
-    "{6}": "|mana_6|",
-    "{7}": "|mana_7|",
-    "{8}": "|mana_8|",
-    "{9}": "|mana_9|",
-    "{10}": "|mana_10|",
-    "{11}": "|mana_11|",
-    "{12}": "|mana_12|",
-    "{13}": "|mana_13|",
-    "{14}": "|mana_14|",
-    "{15}": "|mana_15|",
-    "{16}": "|mana_16|",
-    "{20}": "|mana_20|",
-    "{R/G}": "|mana_rg|",
-    "{R/W}": "|mana_rw|",
-    "{U/B}": "|mana_ub|",
-    "{U/R}": "|mana_ur|",
-    "{W/B}": "|mana_wb|",
-    "{W/U}": "|mana_wu|",
-    "{B/G}": "|mana_bg|",
-    "{B/R}": "|mana_br|",
-    "{G/W}": "|mana_gw|",
-    "{G/U}": "|mana_gu|",
-    "\n": "\n\n   ",
-    "||": "|\ |",
-}
+from toolbox.mtg_vars import symbols_map, card_template
 
 
 def reformat_card_text(text, card_name=None):
