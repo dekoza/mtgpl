5307852ec10ec212fe4d972951073c5954ecfd46
Dominik Kozaczko <dominik@kozaczko.info>
2021-09-04 15:06:56 +0200

    Aktualizacja

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.8.23',
+    VERSION: '2021.9.3',
     LANGUAGE: 'pl',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
